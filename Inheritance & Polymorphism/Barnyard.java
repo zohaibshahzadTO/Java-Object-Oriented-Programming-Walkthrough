76ce48cf1b4571eb38832c6bdd57175f1e3cfc37
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-02-07 12:32:37 -0500

    Working on Inheritance mini program

--- Inheritance & Polymorphism/Barnyard.java
+++ Inheritance & Polymorphism/Barnyard.java
@@ -5,3 +5,34 @@ are given here as static nested classes, but they might as well be separate top 
 that defines the common behaviour of each animal. Cannot be used to create objects, but serves as abstract parameter type
 for polymorphic methods that works for all animals.
 */
+
+public abstract static class Animal {
+  private static int count = 0; // counter sharted by all animals
+  public static int getCount() { return count; }
+
+  public Animal() {
+    System.out.println("Default constructor of Animal"); // for educational purposes
+    count++;
+  }
+
+  public abstract String getSound();
+  public abstract String getSpecies();
+
+  @Override public String toString() {
+    return getSpecies() + " that says " + getSound();
+  }
+}
+
+// A concrete subclass of the previous abstract superclass
+
+public static class Cat extends Animal {
+  private static Random rng = new Random();
+  public Cat() {
+    System.out.println("Default constructor of Cat");
+  }
+
+  @Override public String getSound() {
+    return rng.nextInt(100) < 50? "meow": "purrr";
+  }
+  @Override public String getSpecies(){ return "cat"; }
+}
